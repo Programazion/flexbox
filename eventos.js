ce09c2d5800405eba9338b73a391d05f04a53acf
Programazion <programazion1@gmail.com>
2023-07-24 23:10:17 -0500

    Eventos

---
+++ eventos.js
@@ -0,0 +1,30 @@
+console.log("Loading JavaScript Events");
+
+
+function myAction(onclick) {
+    alert(" Bienvenid@s a Programazion Software company");
+    var data= document.getElementById('home');
+    data.style.backgroundColor= "red";
+
+}
+
+function myAction2(onclick) {
+    alert("Estás a punto de conocer mi perfil profesional");
+    var data= document.getElementById('cv');
+    data.style.backgroundColor= "yellow";
+
+}
+
+function myAction3(onclick) {
+    alert(" Bienvenid@s al Home");
+    var data= document.getElementById('home');
+    data.style.backgroundColor= "orange";
+
+}
+
+function myAction4(onclick) {
+    alert(" Bienvenid@s al Home");
+    var data= document.getElementById('home');
+    data.style.backgroundColor= "orange";
+
+}
